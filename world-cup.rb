09672b48d81c6ffe0aa9be26656b936977658cb8
matrix41 <raymondtam@Raymonds-MacBook-Pro.local>
2014-06-23 12:16:09 -0700

    appended some example code from class to help me write a world cup class

--- world-cup.rb
+++ world-cup.rb
@@ -16,3 +16,24 @@ monkey.each do |match|
   end
 end
 puts "==================================="
+
+
+# require 'httparty' # this looks exclusively in gems directory
+# # require_relative 'httparty' # this looks in your current directory
+
+# # response is a string; get returns a JSON-compliant page
+
+
+# class WorldCupFan
+#     def yells_the_outcomes
+#         response = HTTParty.get('http://worldcup.sfg.io/matches/today')
+#         array_of_matches = response.parsed_response
+#         array_of_matches.each do |match|
+#            puts match['status']
+#         end
+#     end
+# end
+
+# fan = WorldCupFan.new
+
+# fan.yells_the_outcomes()
