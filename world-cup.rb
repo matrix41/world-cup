255a104bce05b1b21a165233fd86502d469f65b7
matrix41 <raymondtam@Raymonds-MacBook-Pro.local>
2014-06-23 10:43:22 -0700

    added location,teams

--- world-cup.rb
+++ world-cup.rb
@@ -5,8 +5,8 @@ response = HTTParty.get('http://worldcup.sfg.io/matches/today')
 monkey = response.parsed_response
 
 monkey.each do |match|
-  puts "==================="
-  puts match['match_number']
-  puts "-------------------"
+  puts "=====Match ##{match['match_number']} ============="
+  puts "| location | #{match['location']} |"
+  puts "| teams: #{match['home_team']['country']} vs. #{match['away_team']['country']} |"
 end
 
