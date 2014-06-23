07eb30501f1d42f67f20c721e1bd53637719fe3d
matrix41 <raymondtam@Raymonds-MacBook-Pro.local>
2014-06-23 12:13:50 -0700

    added logic for winner field; minor output format editting

--- world-cup.rb
+++ world-cup.rb
@@ -5,10 +5,14 @@ response = HTTParty.get('http://worldcup.sfg.io/matches/today')
 monkey = response.parsed_response
 
 monkey.each do |match|
-  puts "=====Match ##{match['match_number']} ============="
-  puts "| Location | #{match['location']} |"
+  puts "============ Match ##{match['match_number']} ============"
+  puts "| Location: #{match['location']} |"
   puts "| Teams: #{match['home_team']['country']} vs. #{match['away_team']['country']} |"
   puts "| Status: #{match['status']} |"
-  puts "| Winner: #{match['winner']} |"
+  if ( match['winner'].nil? )
+    puts "| Winner: No Winner "
+  else
+    puts "| Winner: #{match['winner']} |"
+  end
 end
-
+puts "==================================="
